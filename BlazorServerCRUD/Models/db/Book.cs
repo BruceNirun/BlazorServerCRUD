6e05f2c53307e65ad06e99f1117d89d0cafdcaff
BruceNirun19 <nirun.h@planetcomm.com>
2023-06-21 14:22:16 +0700

    Add project files.

---
+++ BlazorServerCRUD/Models/db/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerCRUD.Models.db;
+
+public partial class Book
+{
+    public int BookId { get; set; }
+
+    [Required(ErrorMessage = "กรุณากรอกชื่อหนังสือด้วย!!!")]
+    public string? BookName { get; set; } = null!;
+
+    [Required(ErrorMessage = "กรุณาเลือกประเภทหนังสือ!!!")]
+    public int? CategoryId { get; set; }
+
+    [Required(ErrorMessage = "กรุณาเลือกสำนักพิมพ์!!!")]
+    public int? PublishId { get; set; }
+
+    [Required(ErrorMessage = "กรุณากรอกรหัส ISBN ก่อน!!!")]
+    public string? Isbn { get; set; }
+
+    [Required(ErrorMessage = "กรุณากรอกต้นทุนหนังสิอ!!!")]
+    public double? BookCost { get; set; }
+
+    [Required(ErrorMessage = "กรุณาระบุราคาขาย!!!")]
+    public double? BookPrice { get; set; }
+
+    public virtual Category? Category { get; set; } = null!;
+
+    public virtual Publisher? Publish { get; set; } = null!;
+}
---
+++ BlazorServerCRUD/Models/db/DemoShopContext.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorServerCRUD.Models.db;
+
+public partial class DemoShopContext : DbContext
+{
+    public DemoShopContext()
+    {
+    }
+
+    public DemoShopContext(DbContextOptions<DemoShopContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Book> Books { get; set; }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Publisher> Publishers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=DemoShop;Trusted_Connection=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.HasKey(e => e.BookId).HasName("PK_Bookk");
+
+            entity.ToTable("Book");
+
+            entity.Property(e => e.BookId).ValueGeneratedNever();
+            entity.Property(e => e.BookName).HasMaxLength(100);
+            entity.Property(e => e.Isbn)
+                .HasMaxLength(50)
+                .HasColumnName("ISBN");
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Books)
+                .HasForeignKey(d => d.CategoryId)
+                .HasConstraintName("FK_Book_Category");
+
+            entity.HasOne(d => d.Publish).WithMany(p => p.Books)
+                .HasForeignKey(d => d.PublishId)
+                .HasConstraintName("FK_Book_Publisher");
+        });
+
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.ToTable("Category");
+
+            entity.Property(e => e.CategoryName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Publisher>(entity =>
+        {
+            entity.HasKey(e => e.PublishId);
+
+            entity.ToTable("Publisher");
+
+            entity.Property(e => e.Address).HasMaxLength(200);
+            entity.Property(e => e.ContactName).HasMaxLength(100);
+            entity.Property(e => e.PublishName).HasMaxLength(100);
+            entity.Property(e => e.Telephone)
+                .HasMaxLength(50)
+                .IsFixedLength();
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
